f174c9b047a1677dacd24a1c701143e13d8ebea6
unknown <june9918@o365.skku.edu>
2024-08-19 13:48:15 +0900

    2024.08.19 modo10-4

--- Project1/modo10-3.cpp
+++ Project1/modo10-3.cpp
@@ -1,33 +1,52 @@
-#include <algorithm>
-#include <functional>
 #include <iostream>
+#include <set>
 #include <string>
-#include <vector>
 
-template <typename Iter>
-void print(Iter begin, Iter end) {
-	while (begin != end) {
-		std::cout << "[" << *begin << "] ";
-		begin++;
-	}
-	std::cout << std::endl;
+template <typename T>
+void print_set(std::set<T>& s) {
+    // 셋의 모든 원소들을 출력하기
+    for (const auto& elem : s) {
+        std::cout << elem  << " " << std::endl;
+    }
+}
+class Todo {
+    int priority;
+    std::string job_desc;
+
+public:
+    Todo(int priority, std::string job_desc)
+        : priority(priority), job_desc(job_desc) {}
+
+    bool operator<(const Todo& t) const {
+        if (priority == t.priority) {
+            return job_desc < t.job_desc;
+        }
+        return priority > t.priority;
+    }
+
+    friend std::ostream& operator<<(std::ostream& o, const Todo& td);
+};
+
+std::ostream& operator<<(std::ostream& o, const Todo& td) {
+    o << "[ 중요도: " << td.priority << "] " << td.job_desc;
+    return o;
 }
 
 int main() {
-	std::vector<int> vec;
-	vec.push_back(5);
-	vec.push_back(3);
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(4);
+    std::set<Todo> todos;
 
-	std::cout << "처음 vec 상태 ------" << std::endl;
-	print(vec.begin(), vec.end());
+    todos.insert(Todo(1, "농구 하기"));
+    todos.insert(Todo(2, "수학 숙제 하기"));
+    todos.insert(Todo(1, "프로그래밍 프로젝트"));
+    todos.insert(Todo(3, "친구 만나기"));
+    todos.insert(Todo(2, "영화 보기"));
 
-	std::cout << "벡터 전체에 1 을 더한다" << std::endl;
-	std::transform(vec.begin(), vec.end(), vec.begin(), [](int i) {return i + 1; });
-	print(vec.begin(), vec.end());
+    print_set(todos);
+
+    std::cout << "-------------" << std::endl;
+    std::cout << "숙제를 끝냈다면!" << std::endl;
+    todos.erase(todos.find(Todo(2, "수학 숙제 하기")));
+    print_set(todos);
 }
 
 /*
